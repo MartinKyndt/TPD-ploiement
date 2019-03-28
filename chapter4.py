4df6e4ab8832ddd16f6e5c63079fcce65897d42e
Kyndt Martin <martin.kyndt@insa-lyon.fr>
2019-03-28 09:07:30 +0100

    Version finale du code chapter4.py avec les docstring

--- chapter4.py
+++ chapter4.py
@@ -1,28 +1,48 @@
-import math 
+# -*- coding: utf-8 -*-
+"""Ce code permet de dessiner des étoiles en utilisant l'outil graphique turtle
+"""
+
 import turtle 
-window=turtle.Screen() 
-window.bgcolor("lightgreen")
-tess=turtle.Turtle()
-
-def draw_poly(t,n,sz):
-	for _ in range(n):
-		t.forward(sz)
-		tess.right(360/n)
-
-def draw_equilateraltriangle(animal, size):
-	draw_poly(animal,3,size) 
 
 def draw_stars(animal,size):
+	"""Cette fonction permet de dessiner une étoile via le module turtle de python.
+
+	Parameters 
+	----------
+	animal : l'objet de type turtle
+	size : la taille des côtés de l'étoile
+
+	Returns 
+	-------
+	Ne retourne rien
+	"""
 	for _ in range(5):	
 		animal.forward(size)
 		animal.right(144)
 
-for _ in range(5) :
-	draw_stars(tess,100)
-	tess.penup()
-	tess.forward(350)
-	tess.right(144)
-	tess.pendown()
+def draw_five_stars () : 
+	"""Cette fonction permet de dessiner cinq étoiles grâce à la fonction précédente.
+
+	Parameters 
+	----------
+	Aucuns
+
+	Returns 
+	-------
+	Ne retourne rien
+	"""
+	for _ in range(5) :
+		draw_stars(tess,100)
+		tess.penup()
+		tess.forward(350)
+		tess.right(144)
+		tess.pendown()
+
+
+window=turtle.Screen() 
+window.bgcolor("lightgreen")
+tess=turtle.Turtle()
+draw_five_stars()
 
 
 
